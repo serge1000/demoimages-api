16314d8eeb5cd2636a8210fc6c44a9abfbc81ff4
Name <1111@1111.com>
2021-07-20 16:46:34 +0300

    all: app +  2 servers

--- server.js
+++ server.js
@@ -1,54 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
 const { response } = require('express');
-//const request = require('request');
+const WebSocket = require("ws");
+const cors = require('cors');
 
 const app = express();
 
 app.use(cors())
+
 app.use(bodyParser.urlencoded({
     extended: true
   }));
 app.use(bodyParser.json());
 
-const returnImages = async (res, imageNumber) => {
-  let imagedata;
-  const results = [];
-
-  for (let i = 0; i < imageNumber; i++) {
-    const robotID = (Math.floor(Math.random() * 1000) + 1).toString();
-    try {
-      axiosres = (await axios.get('https://robohash.org/'+ robotID , {
-        responseType: 'arraybuffer'
-      }));
-    }
-    catch(e) {
-      console.log('Catch an error: ', e);
-    }
-
-    const imageobj = {
-      imagedata: Buffer.from(axiosres.data).toString('base64'),
-      robotID:   robotID
-    }
-    results.push(imageobj);
-  }
-  res.send(results);
-}
-
-
-
-app.get('/getsearchimages', (req, res)=> { 
-  returnImages(res,3);
+app.get('/gettestimages', (req, res)=> { 
+  results = requestSearchServer("GET_TEST_IMAGES", "", res);
 });
 
-app.get('/getfoundimages', (req, res)=> { 
-  const imageNumber = (Math.floor(Math.random() * 10) + 1).toString();
-  returnImages(res, imageNumber);
+app.get('/searchrequest/:id', (req, res)=> { 
+    results = requestSearchServer("SEARCH_REQUEST", req.params.id.toString(), res);
 });
-
 
 app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
+
   })
+
+// Request search server
+function requestSearchServer(event, imageId, res){
+    const ws = new WebSocket("ws://localhost:8082");
+    ws.addEventListener("open", () =>{
+        data = JSON.stringify({
+          event: event,
+          data: imageId
+        })
+        ws.send(data);
+    });
+
+    ws.addEventListener("message", ({data}) =>{
+      const object = JSON.parse(data);
+      res.send(JSON.stringify(object));
+    });
+
+  }
